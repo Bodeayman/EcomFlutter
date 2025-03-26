8f65bc2afe307f8694d8e85b6a0d53afc6c87955
Abdullah Ayman <abdulluhayman@gmail.com>
2025-03-26 16:46:10 +0200

    Implemented the api service

--- lib/utils/stripe_service.dart
+++ lib/utils/stripe_service.dart
@@ -1,12 +1,25 @@
-import 'package:dio/dio.dart';
 import 'package:ecomflutter/model/payment_intent_input_model.dart';
 import 'package:ecomflutter/model/payment_intent_model/payment_intent_model.dart';
+import 'package:ecomflutter/utils/api_key.dart';
+import 'package:ecomflutter/utils/api_service.dart';
 
 class StripeService {
-  PaymentIntentModel createPaymentIntent(
+  final ApiService apiService = ApiService();
+  Future<PaymentIntentModel> createPaymentIntent(
     PaymentIntentInputModel paymentIntentInputModel,
-  ) {
-    return PaymentIntentModel();
+  ) async {
+    var response = await apiService.post(
+      body: paymentIntentInputModel.toJson(),
+      url: 'https://api.stripe.com/v1/payment_intents',
+      token: secretKey,
+    );
+    PaymentIntentModel tempPaymentIntentModel = PaymentIntentModel.fromJson(
+      response.data,
+    );
+    // Takes the response from the api as json and convert it to the model so you can use it
+
+    return tempPaymentIntentModel;
   }
 }
+
 //51
